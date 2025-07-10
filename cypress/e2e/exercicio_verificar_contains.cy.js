b45aa81490fafb2edb8910f5ab98fee81c75df43
Vitor Fantin <fantinfx@gmail.com>
2025-07-10 20:47:23 -0300

    correcao exercicio_verificar_contains

--- cypress/e2e/exercicio_verificar_contains.cy.js
+++ cypress/e2e/exercicio_verificar_contains.cy.js
@@ -1,16 +1,17 @@
 describe('Verificar Contains', () => {
-    before(() => {
+beforeEach(() => {
         cy.visit('https://adopet-frontend-cypress.vercel.app/');
+       
     });
     it('Teste titulo', () => {
-        cy.title().should('eq', 'Adopet');
+       cy.title().should('eq', 'AdoPet')
     
     });
     it('Verifica mensagem de texto', () => {
-        cy.contais('p', 'Quem ama adota!').should('be.visible');
+        cy.contains('.initial > p', 'Adotar pode mudar uma vida. Que tal buscar seu novo melhor amigo hoje? Vem com a gente!').should('be.visible');
     });
     it('Verifica mensagem de texto', () => {
-        cy.contais('p', 'Adotar pode mudar umaAdotar pode mudar uma vida. Que tal buscar seu novo melhor amigo hoje? Vem com a gente!').should(be.visible);
+        cy.contains('.footer','Quem ama adota!').should('be.visible');
         
     });
 });
