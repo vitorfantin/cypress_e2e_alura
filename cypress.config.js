8385b71b9c5a38ef242aabe8bc087691c51a19df
Vitor Fantin <fantinfx@gmail.com>
2025-07-12 18:50:14 -0300

    testando geracao relatorio mochawesome e cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+   projectId: "xd38yv",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    // video: true,
+    video: true,
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: false,
+      html: true,
+      json: false,
+      timestamp: "mmddyyyy_HHMMss" }
   },
 });
